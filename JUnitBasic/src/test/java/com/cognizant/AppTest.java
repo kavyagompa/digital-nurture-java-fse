89823168de180183941aab98fc41412a540ea25e
Kavyasree Gompa <kavyagompa80@gmail.com>
2026-06-25 00:59:20 +0530

    Add files via upload

---
+++ JUnitBasic/src/test/java/com/cognizant/AppTest.java
@@ -0,0 +1,12 @@
+package com.cognizant;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void testSample() {
+        assertEquals(5, 2 + 3);
+    }
+}
