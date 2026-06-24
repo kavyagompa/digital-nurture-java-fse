5d03f64d6ce30914966838198e80dd087adfd56f
Kavyasree Gompa <kavyagompa80@gmail.com>
2026-06-25 02:12:19 +0530

    Add files via upload

---
+++ MockitoMockingExample/src/test/java/MyServiceTest.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class MyServiceTest {
+
+    @Test
+    public void testExternalApi() {
+
+        ExternalApi mockApi = mock(ExternalApi.class);
+
+        when(mockApi.getData()).thenReturn("Mock Data");
+
+        MyService service = new MyService(mockApi);
+
+        String result = service.fetchData();
+
+        assertEquals("Mock Data", result);
+    }
+}
