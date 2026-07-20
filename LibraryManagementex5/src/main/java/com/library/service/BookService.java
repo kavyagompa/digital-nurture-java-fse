0265c211f61f285785a35bbf61a0114a527bfb41
Kavyasree Gompa <kavyagompa80@gmail.com>
2026-07-20 21:14:03 +0530

    Add files via upload

---
+++ LibraryManagementex5/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,29 @@
+package com.library.service;
+
+
+import com.library.repository.BookRepository;
+
+
+public class BookService {
+
+
+    private BookRepository bookRepository;
+
+
+    // Setter Dependency Injection
+    public void setBookRepository(BookRepository bookRepository) {
+
+        this.bookRepository = bookRepository;
+
+    }
+
+
+    public void display() {
+
+        System.out.println("Book Service is working");
+
+        bookRepository.display();
+
+    }
+
+}
