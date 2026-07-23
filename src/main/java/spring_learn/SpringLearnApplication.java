18bfa07ba2df3e5436785db3bf395d43b0a6f895
kavyagompa <kavyagompa80@gmail.com>
2026-07-23 23:21:12 +0530

    Added SimpleDateFormat hands-on code back

--- src/main/java/spring_learn/SpringLearnApplication.java
+++ src/main/java/spring_learn/SpringLearnApplication.java
@@ -1,13 +1,50 @@
 package spring_learn;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class SpringLearnApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringLearnApplication.class, args);
+
+        // Previous hands-on code
+        displayDate();
+
+    }
+
+
+    public static void displayDate() {
+
+        // Loading Spring XML configuration
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("date-format.xml");
+
+
+        // Getting SimpleDateFormat bean
+        SimpleDateFormat format =
+                context.getBean("dateFormat", SimpleDateFormat.class);
+
+
+        try {
+
+            Date date = format.parse("31/12/2018");
+
+            System.out.println("Parsed Date: " + date);
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+
     }
 
 }
