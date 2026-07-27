24d7354126af16ee604e6c2905b0431ca6c497f7
Kavyasree Gompa <kavyagompa80@gmail.com>
2026-07-28 02:30:17 +0530

    Add files via upload

---
+++ spring-learn (2)/spring-learn/src/test/java/com/cognizant/springlearn/SpringLearnApplicationTests.java
@@ -0,0 +1,17 @@
+package com.cognizant.springlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearnApplicationTests {
+
+
+    @Test
+    void contextLoads() {
+
+        System.out.println("Spring Context Loaded Successfully");
+
+    }
+
+}
