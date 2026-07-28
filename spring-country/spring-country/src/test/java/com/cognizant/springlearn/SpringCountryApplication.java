275e52d793f506b5d29cbb2f6e343a221e1749c8
Kavyasree Gompa <kavyagompa80@gmail.com>
2026-07-28 09:41:34 +0530

    Add files via upload

---
+++ spring-country/spring-country/src/test/java/com/cognizant/springlearn/SpringCountryApplication.java
@@ -0,0 +1,37 @@
+package com.cognizant.springlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+@SpringBootApplication
+public class SpringCountryApplication {
+
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringCountryApplication.class, args);
+
+        displayCountry();
+
+    }
+
+
+    public static void displayCountry() {
+
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+
+        Country country =
+                context.getBean("country", Country.class);
+
+
+        System.out.println("Country : " + country);
+
+    }
+
+}
